231972062ead532c42a91e8c4bf78cb20a12c2d6
pspness420 <tunahan.keser@student.ipso.ch>
2025-02-25 19:54:32 +0100

    mongodb connection

--- SkiServiceManagement/Models/Serviceauftrag.cs
+++ SkiServiceManagement/Models/Serviceauftrag.cs
@@ -1,35 +1,43 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using System;
 using System.ComponentModel.DataAnnotations;
-using System.Threading.Tasks;
 
 namespace SkiServiceManagement.Models
 {
     public class Serviceauftrag
     {
-        [Key]
-        public int Id { get; set; }
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)] // MongoDB ObjectId as string
+        public string Id { get; set; }
+
         [Required]
         [MaxLength(100)]
         public string KundenName { get; set; }
+
         [Required]
         [MaxLength(100)]
         public string Email { get; set; }
+
         [MaxLength(50)]
         public string Telefon { get; set; }
+
         [Required]
         [MaxLength(30)]
         public string Prioritaet { get; set; }
+
         [Required]
         [MaxLength(250)]
         public string Dienstleistung { get; set; }
+
         [Required]
-        public DateTime CreateDate { get; set; } = DateTime.Now;
+        public DateTime CreateDate { get; set; } = DateTime.UtcNow;
+
         [Required]
         public DateTime PickupDate { get; set; }
+
         [Required]
         [MaxLength(100)]
         public string Status { get; set; } = "Offen";
     }
-
-
 }
